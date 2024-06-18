db4bc2db0f0259bb47afd76477c10d658aa342a7
Ashley-Zhao <ashley.zhao@ibm.com>
2024-06-18 15:12:24 -0400

    https://www.youtube.com/watch?v=-yVxChp7HoQ&t=3645s working Kafka

--- go.mod
+++ go.mod
@@ -5,7 +5,12 @@ go 1.21.4
 require rsc.io/quote v1.5.2
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20231202071711-9a357b53e9c9 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0 // indirect
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,11 +4,62 @@ import (
 	"fmt"
 	"log"
 	"time"
-	"rsc.io/quote"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	topic := "HVSE"
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         "foo",
+		"acks":              "all",
+	})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+	}
+
+	go func() {
+		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers": "localhost:9092",
+			"group.id":          "foo",
+			"auto.offset.reset": "smallest",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = consumer.Subscribe(topic, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			ev := consumer.Poll(100)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("Consumed message from queue: %s\n", string(e.Value))
+			case *kafka.Error:
+				fmt.Printf("%v\n", e)
+			}
+
+		}
+	}()
+
+	deliverch := make(chan kafka.Event, 10000)
+	for {
+		err = p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte("FOO")},
+			deliverch,
+		)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		<-deliverch
+		time.Sleep(time.Second * 3)
+	}
+
 }
