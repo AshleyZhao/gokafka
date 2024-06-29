122aaec3adef02cc7d8a81028a8e519e66021b76
Ashley-Zhao <ashley.zhao@ibm.com>
2024-06-29 10:21:41 -0400

    https://www.youtube.com/watch?v=-yVxChp7HoQ&t=3645s

--- main.go
+++ main.go
@@ -43,7 +43,6 @@ func (op *OrderPlacer) placeOrder(orderType string, size int) error {
 }
 
 func main() {
-	topic := "HVSE"
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
 		"client.id":         "foo",
@@ -53,33 +52,6 @@ func main() {
 	if err != nil {
 		fmt.Printf("Failed to create producer: %s\n", err)
 	}
-
-	go func() {
-		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-			"bootstrap.servers": "localhost:9092",
-			"group.id":          "foo",
-			"auto.offset.reset": "smallest",
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = consumer.Subscribe(topic, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		for {
-			ev := consumer.Poll(100)
-			switch e := ev.(type) {
-			case *kafka.Message:
-				fmt.Printf("Consumed message from queue: %s\n", string(e.Value))
-			case *kafka.Error:
-				fmt.Printf("%v\n", e)
-			}
-
-		}
-	}()
 
 	op := NewOrderPlacer(p, "HVSE")
 
